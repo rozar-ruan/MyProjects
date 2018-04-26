6592f4719c1f5648fa6cc9394bd6f5d7eb18c2b5
阮朝 <1542666278@qq.com>
2018-04-26 20:27:54 +0800

    增加vue登陆

---
+++ VueJuniorDemo/VueJuniorDemo/js/loginPage/loginApp.js
@@ -0,0 +1,14 @@
+﻿///<reference path="../lib/vue.js"/>
+var loginFormApp = new Vue({
+    el: "#loginApp",
+    data: {
+        name: "",
+        pwd:""
+    },
+    methods: {
+        doLogin: function () {
+            alert("name:" + this.name + ";pwd:" + this.pwd);
+            // 在此处提交给后台
+        }
+    }
+});
